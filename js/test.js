c1f061c63d50bd8ce17b018d347a5a27051ece9b
ecmjohnson <ecmjohnson@hotmail.com>
2018-03-31 11:50:11 -0400

    api request

--- js/test.js
+++ js/test.js
@@ -1,1 +1,23 @@
-alert("this is a test");
+
+function submit_code(program_text)
+{
+    var request = require('request');
+
+    var program = {
+        script : "",
+        language: "php",
+        versionIndex: "0",
+        clientId: "YourClientID",
+        clientSecret:"YourClientSecret"
+    };
+    request({
+        url: 'https://api.jdoodle.com/execute',
+        method: "POST",
+        json: program
+    },
+    function (error, response, body) {
+        console.log('error:', error);
+        console.log('statusCode:', response && response.statusCode);
+        console.log('body:', body);
+    });
+}
