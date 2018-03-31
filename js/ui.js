9c9833dbb2ff49ed9b6af6772ba0e4e4e59fe8a1
Monty1029 <dhanani.monty@gmail.com>
2018-03-31 14:13:12 -0400

    added timer

---
+++ js/ui.js
@@ -0,0 +1,25 @@
+function grab_code() {
+    var s = document.getElementById("codeTextArea").value;
+    console.log(s);
+    alert(s);
+    submit_code(s);
+}
+
+window.onload = function() {
+    var seconds = 60;
+    var mins = 1
+    function tick() {
+        var counter = document.getElementById("timer");
+        var current_minutes = mins-1
+        seconds--;
+        counter.innerHTML = current_minutes.toString() + ":" + (seconds < 10 ? "0" : "") + String(seconds);
+        if( seconds > 0 ) {
+            setTimeout(tick, 1000);
+        } else {
+            if(mins > 1){
+                countdown(mins-1);           
+            }
+        }
+    }
+    tick();
+}
