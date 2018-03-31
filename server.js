3e67b02b8c87cfa929295d1b179afd236624499e
ecmjohnson <ecmjohnson@hotmail.com>
2018-03-31 16:11:16 -0400

    add new things

--- server.js
+++ server.js
@@ -1,24 +1,14 @@
 var http = require('http');
-var fs = require('fs');
-var express = require('express');
-var app = express();
-app.post('handle', function(request,response){
-    console.log(request.body);
-});
-app.use(express.static(__dirname))
-app.listen(8080, () => {console.log("listening on port")})
 
 require('./js/test.js')
 
-// const PORT=8080;
+var express = require('express');
+var app = express();
 
-// fs.readFile('./index.html', function (err, html) {
+app.post('/', function(req,res){
+    res.send("html");
+    console.log(req);
+});
 
-//     if (err) throw err;    
-
-//     http.createServer(function(request, response) {  
-//         response.writeHeader(200, {"Content-Type": "text/html"});  
-//         response.write(html);  
-//         response.end();  
-//     }).listen(PORT);
-// });
+app.use(express.static(__dirname))
+app.listen(8080, () => {console.log("Server is now running...")})
