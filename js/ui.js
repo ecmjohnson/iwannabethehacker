19ef047242749705b1f9d7a4be4c5b06968bcf35
Monty1029 <dhanani.monty@gmail.com>
2018-03-31 16:26:12 -0400

    added failure and success pages

--- js/ui.js
+++ js/ui.js
@@ -7,11 +7,14 @@ function grab_code() {
 
 window.onload = function() {
     var seconds = 60;
-    var mins = 1
+    var mins = 1;
     function tick() {
         var counter = document.getElementById("timer");
-        var current_minutes = mins-1
+        var current_minutes = mins-1;
         seconds--;
+        if (seconds == 0 && current_minutes == 0) {
+            window.location.replace("fail.html");
+        }
         counter.innerHTML = current_minutes.toString() + ":" + (seconds < 10 ? "0" : "") + String(seconds);
         if( seconds > 0 ) {
             setTimeout(tick, 1000);
@@ -19,7 +22,7 @@ window.onload = function() {
             if(mins > 1){
                 countdown(mins-1);           
             }
-        }
+        }        
     }
     tick();
 }
