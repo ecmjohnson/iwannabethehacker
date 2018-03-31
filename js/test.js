9ff8c079a023bda14be5012e3262c216d55b575d
ecmjohnson <ecmjohnson@hotmail.com>
2018-03-31 14:06:20 -0400

    first working with node.js

--- js/test.js
+++ js/test.js
@@ -1,35 +1,53 @@
 submit_code("test");
-alert("did a thing (please check console)");
 
 function submit_code(program_text)
 {
-    // Fetch from a placeholder URL for testing
-    var test_url = 'https://api.jdoodle.com/execute';
+    // // Fetch from a placeholder URL for testing
+    // var test_url = 'https://api.jdoodle.com/execute';
 
-    // Request listener function
-    function reqListener() {
-        var data = JSON.parse(this.responseText);
-        console.log(data);
-      }
+    // // Request listener function
+    // function reqListener() {
+    //     var data = JSON.parse(this.responseText);
+    //     console.log(data);
+    //   }
       
-      // Error function
-      function reqError(err) {
-        console.log('Fetch Error :-S', err);
-      }
+    //   // Error function
+    //   function reqError(err) {
+    //     console.log('Fetch Error :-S', err);
+    //   }
       
-      // Make the request
-      var oReq = new XMLHttpRequest({mozSystem: true});
-      oReq.onload = reqListener;
-      oReq.onerror = reqError;
-      oReq.open('post', test_url, true);
-      oReq.setRequestHeader("Content-type", "application/json");
-      var data = JSON.stringify({
-          "script" : "print('hello')",
-          "language" : "python",
-          "versionIndex" : "0",
-          "clientId" : "8f618a541abc0980378440af84c904a6",
-          "clientSecret" : "d75f203ec72e17920299fdbf06690017444dccb01704e469534ab06586543665"
-      });
-      oReq.send(data);
-      
+    //   // Make the request
+    //   var oReq = new XMLHttpRequest({mozSystem: true});
+    //   oReq.onload = reqListener;
+    //   oReq.onerror = reqError;
+    //   oReq.open('post', test_url, true);
+    //   oReq.setRequestHeader("Content-type", "application/json");
+    //   var data = JSON.stringify({
+    //       "script" : "print('hello')",
+    //       "language" : "python",
+    //       "versionIndex" : "0",
+    //       "clientId" : "8f618a541abc0980378440af84c904a6",
+    //       "clientSecret" : "d75f203ec72e17920299fdbf06690017444dccb01704e469534ab06586543665"
+    //   });
+    //   oReq.send(data);
+
+    var request = require('request');
+
+    var program = {
+        script : "print('hello')",
+        language: "python",
+        versionIndex: "0",
+        clientId: "8f618a541abc0980378440af84c904a6",
+        clientSecret:"d75f203ec72e17920299fdbf06690017444dccb01704e469534ab06586543665"
+    };
+    request({
+        url: 'https://api.jdoodle.com/execute',
+        method: "POST",
+        json: program
+    },
+    function (error, response, body) {
+        console.log('error:', error);
+        console.log('statusCode:', response && response.statusCode);
+        console.log('body:', body);
+    });
 }
