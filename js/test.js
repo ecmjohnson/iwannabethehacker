7bcb052ddf58fa66350e652ac62b5778b4ddc6ef
ecmjohnson <ecmjohnson@hotmail.com>
2018-03-31 13:29:13 -0400

    added a functional block for submit_code

--- js/test.js
+++ js/test.js
@@ -4,21 +4,32 @@ alert("did a thing (please check console)");
 function submit_code(program_text)
 {
     // Fetch from a placeholder URL for testing
-    var test_url = 'https://jsonplaceholder.typicode.com/posts/1';
-    fetch(test_url)
-    .then(
-        function(response) {
-        if (response.status !== 200) {
-            console.log('Looks like there was a problem. Status Code: ' +
-            response.status);
-            return;
-        }
-        response.json().then(function(data) {
-            console.log(data);
-        });
-        }
-    )
-    .catch(function(err) {
+    var test_url = 'https://api.jdoodle.com/execute';
+
+    // Request listener function
+    function reqListener() {
+        var data = JSON.parse(this.responseText);
+        console.log(data);
+      }
+      
+      // Error function
+      function reqError(err) {
         console.log('Fetch Error :-S', err);
-    });
+      }
+      
+      // Make the request
+      var oReq = new XMLHttpRequest({mozSystem: true});
+      oReq.onload = reqListener;
+      oReq.onerror = reqError;
+      oReq.open('post', test_url, true);
+      oReq.setRequestHeader("Content-type", "application/json");
+      var data = JSON.stringify({
+          "script" : "print('hello')",
+          "language" : "python",
+          "versionIndex" : "0",
+          "clientId" : "8f618a541abc0980378440af84c904a6",
+          "clientSecret" : "d75f203ec72e17920299fdbf06690017444dccb01704e469534ab06586543665"
+      });
+      oReq.send(data);
+      
 }
