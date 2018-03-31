35fb17a7c07c65e3035ec42360417cab2cfdf8b5
ecmjohnson <ecmjohnson@hotmail.com>
2018-03-31 14:10:59 -0400

    functional python2 evaluator

--- js/test.js
+++ js/test.js
@@ -1,41 +1,12 @@
-submit_code("test");
+submit_code("print('hello')");
 
 function submit_code(program_text)
 {
-    // // Fetch from a placeholder URL for testing
-    // var test_url = 'https://api.jdoodle.com/execute';
-
-    // // Request listener function
-    // function reqListener() {
-    //     var data = JSON.parse(this.responseText);
-    //     console.log(data);
-    //   }
-      
-    //   // Error function
-    //   function reqError(err) {
-    //     console.log('Fetch Error :-S', err);
-    //   }
-      
-    //   // Make the request
-    //   var oReq = new XMLHttpRequest({mozSystem: true});
-    //   oReq.onload = reqListener;
-    //   oReq.onerror = reqError;
-    //   oReq.open('post', test_url, true);
-    //   oReq.setRequestHeader("Content-type", "application/json");
-    //   var data = JSON.stringify({
-    //       "script" : "print('hello')",
-    //       "language" : "python",
-    //       "versionIndex" : "0",
-    //       "clientId" : "8f618a541abc0980378440af84c904a6",
-    //       "clientSecret" : "d75f203ec72e17920299fdbf06690017444dccb01704e469534ab06586543665"
-    //   });
-    //   oReq.send(data);
-
     var request = require('request');
 
     var program = {
-        script : "print('hello')",
-        language: "python",
+        script : program_text,
+        language: "python2",
         versionIndex: "0",
         clientId: "8f618a541abc0980378440af84c904a6",
         clientSecret:"d75f203ec72e17920299fdbf06690017444dccb01704e469534ab06586543665"
