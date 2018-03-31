4fd762289974d1d8e324f6b0950e0d08df01c54b
ecmjohnson <ecmjohnson@hotmail.com>
2018-03-31 12:07:38 -0400

    adding test api request

--- js/test.js
+++ js/test.js
@@ -3,23 +3,23 @@ alert("did a thing (please check console)");
 
 function submit_code(program_text)
 {
-    var request = require('request');
-
-    var program = {
-        script : "",
-        language: "php",
-        versionIndex: "0",
-        clientId: "YourClientID",
-        clientSecret:"YourClientSecret"
-    };
-    request({
-        url: 'https://api.jdoodle.com/execute',
-        method: "POST",
-        json: program
-    },
-    function (error, response, body) {
-        console.log('error:', error);
-        console.log('statusCode:', response && response.statusCode);
-        console.log('body:', body);
+    // Fetch from a placeholder URL for testing
+    var test_url = 'https://jsonplaceholder.typicode.com/posts/1';
+    fetch(test_url)
+    .then(
+        function(response) {
+        if (response.status !== 200) {
+            console.log('Looks like there was a problem. Status Code: ' +
+            response.status);
+            return;
+        }
+        response.json().then(function(data) {
+            console.log(data);
+        });
+        }
+    )
+    .catch(function(err) {
+        console.log('Fetch Error :-S', err);
     });
+
 }
